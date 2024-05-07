58ccd409224ce213c2e0debe0a75604032bc1484
Heeju <62732461+JOOZOO20@users.noreply.github.com>
2024-05-08 04:45:39 +0900

    전반적인 역할을 담당하는 HomeController #4

---
+++ Enchive/HomeController.java
@@ -0,0 +1,20 @@
+package net.skhu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+
+
+	@GetMapping("/index")
+	public String Home(Model model) {
+
+		return "index";
+	}
+
+
+
+}
