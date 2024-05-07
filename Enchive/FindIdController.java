c609ced6dd259ecf86ea0192da58ad6666249d87
Heeju <62732461+JOOZOO20@users.noreply.github.com>
2024-05-08 04:42:37 +0900

    아이디 및 비밀번호 찾는 컨트롤러
    아이디 및 비밀번호 찾는 컨트롤러 #8

---
+++ Enchive/FindIdController.java
@@ -0,0 +1,17 @@
+package net.skhu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class FindIdController {
+
+	@GetMapping("/findId")
+	public String Home(Model model) {
+
+		return "findId";
+	}
+
+}
